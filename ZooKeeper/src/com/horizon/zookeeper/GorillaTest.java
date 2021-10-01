684601db6fd942e1ba3435de8ebaff0d8dee8cb0
Chris <christhehorizon@gmail.com>
2021-09-30 21:17:40 -0700

    ZooKeeper assignment

---
+++ ZooKeeper/src/com/horizon/zookeeper/GorillaTest.java
@@ -0,0 +1,27 @@
+package com.horizon.zookeeper;
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		Gorilla kong = new Gorilla();
+		kong.throwSomething();
+		kong.throwSomething();
+		kong.throwSomething();
+		kong.eatBananas();
+		kong.eatBananas();
+		kong.climb();
+		kong.displayEnergy();
+		
+		Bat bird = new Bat();
+		bird.attackTown();
+		bird.attackTown();
+		bird.attackTown();
+		bird.eatHumans();
+		bird.eatHumans();
+		bird.fly();
+		bird.fly();
+		bird.displayEnergy();
+
+	}
+	
+}
