8937695ef2fe6415d9fd819708b63b5f6f0e5632
Chris <christhehorizon@gmail.com>
2021-10-02 21:39:58 -0700

    Daikichi assignment complete

---
+++ Daikichi/src/main/java/com/horizon/daikichiroutes/DaikichiController.java
@@ -0,0 +1,26 @@
+package com.horizon.daikichiroutes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiController {
+	
+	// home screen
+	@RequestMapping("")
+	public String index() {
+		return "Welcome";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+
+}
